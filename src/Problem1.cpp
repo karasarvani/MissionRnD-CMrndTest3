b1cd7917019f4452d1bd2d8597d454f0f5a4dd64
karasarvani <karasarvani@missionrnd.com>
2016-04-24 12:20:40 +0530

    Submit Commit

--- src/Problem1.cpp
+++ src/Problem1.cpp
@@ -1,27 +1,27 @@
 /*
-Given a Binary Tree of N Nodes, which have unique values in the range of 
+Given a Binary Tree of N Nodes, which have unique values in the range of
 [0,N](Inclusive)  with one value(Node) missing in that sequence.
 Return that missing value .
 
-Example 1 : N=5 
-   3
-  / \
- 4   1
-    / \
-   5   0
+Example 1 : N=5
+3
+/ \
+4   1
+/ \
+5   0
 Missing Value is 2 ,Sequence is [0,1,2,3,4,5]
 
 Example 2 : N=8
-      2
-     / \
-    4   5
-   / \ / \
-  3  1 6  8
- /
+2
+/ \
+4   5
+/ \ / \
+3  1 6  8
+/
 0
 Missing Value is 7 ,Sequence is [0,1,2,3,4,5,6,7,8]
 
-Constaints : 
+Constaints :
 -10000<=N<=10000 [N will not be equal to 0]
 
 
@@ -30,7 +30,7 @@ Input :
 Struct node Root Address
 Output :
 Return Missing Value .
-Return -1 for NULL Inputs 
+Return -1 for NULL Inputs
 
 Note:
 Dont Create an Extra Array/String ,or else your solution would receive 30% Cut in the Marks you got for this
@@ -50,7 +50,20 @@ struct node{
 	struct node *right;
 };
 
-
-int get_missing_value(struct node *root,int n){
-    return -1;
+void preorder(struct node* root, int * sum)
+{
+	if (root != NULL)
+	{
+		(*sum) = (*sum) + root->data;
+		preorder(root->left, sum);
+		preorder(root->right, sum);
+	}
+}
+int get_missing_value(struct node *root, int n){
+	int sum = 0;
+	if (root == NULL || n <= 0)
+		return -1;
+	else
+		preorder(root, &sum);
+	return((n*(n + 1)) / 2) - sum;
 }
