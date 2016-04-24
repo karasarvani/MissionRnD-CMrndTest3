4b7ea7d3c954c6a3e4607c11ab4d7b9349c503ce
karasarvani <karasarvani@missionrnd.com>
2016-04-24 12:30:19 +0530

    Submit Commit

--- src/Problem2.cpp
+++ src/Problem2.cpp
@@ -3,7 +3,7 @@ Given a Sorted Doubly Linked List and a Binary Search Tree , Check if they are i
 The DLL will have nodes of type node_dll
 Note :
 A DLL can be represented as a BST in many forms ,And if a dll and any one of the forms is given as input you
-need to return 1 otherwise 0. 
+need to return 1 otherwise 0.
 
 ->Dont Create an Extra Array ,or else your solution would receive 20% Cut in the Marks you got .
 
@@ -12,43 +12,43 @@ Examples :
 Example 0:
 DLL:1->5->8
 BST(All 3 return 1):
-    5         8      1
-   / \       /        \
-  1   8     5          5
-           /            \
-          1              8
+5         8      1
+/ \       /        \
+1   8     5          5
+/            \
+1              8
 Returns 1
-    8
-   / \
-  5   1
+8
+/ \
+5   1
 Returns 0
 Example 1:
 DLL : 1->5
-BST : 
+BST :
 5
- \
- 10
+\
+10
 Returns 0
 
 Example 2:
 DLL:1->3->5-9->11
 BST:
-    5
-   / \
-  1   11
-   \  /      
-    3 9
+5
+/ \
+1   11
+\  /
+3 9
 
 Returns 1
 
 Example 3:
 DLL: 1->5->7->10->12->15->20
 BST:
-        10
-        /\
-       5  15
-      /\  / \
-     1  7 12 20
+10
+/\
+5  15
+/\  / \
+1  7 12 20
 Returns 1
 
 Note 2 :
@@ -70,7 +70,50 @@ struct node{
 	struct node *left;
 	struct node *right;
 };
+void inorder(struct node*root, int *a, int *i)
+{
+	if (root != NULL)
+	{
+		inorder(root->left, a, i);
+		a[(*i)++] = root->data;
+		inorder(root->right, a, i);
+	}
+}
+
+void inorder_t(struct node * root,int *a)
+{
+
+
+	int i = 0;
+	inorder(root, a, &i);
+}
 
 int is_identical(struct node_dll *head, struct node *root){
-	return -1;
+	if (head == NULL || root == NULL)
+		return -1;
+	int *arr;
+	int len = 0;
+	struct node_dll *temp=head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		len++;
+	}
+	arr = (int*)malloc(len*sizeof(int));
+	inorder_t(root, arr);
+	int i = 0;
+	temp = head;
+	while (temp != NULL)
+	{
+		if (temp->data == arr[i])
+		{
+			i++;
+			temp = temp->next;
+		}
+		else
+			break;
+	}
+	if (i == len)
+		return 1;
+	else return 0;
 }
